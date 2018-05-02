b9eeb0a96ae3c8075db3946557baae3aea535762
Unknown <kanakana0116@icloud.com>
2018-05-02 17:54:45 +0900

    最新のコミット

---
+++ Sanple/Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Heloo,worldをコンソールウィンドウに表示する
+        Debug.Log("Heloo,world");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
